2ea23d29d802d91d98ba398c86264f4aa18a95e5
red_user <red@mail>
2026-03-25 15:18:15 +0300

    r0

---
+++ F.java
@@ -0,0 +1,30 @@
+public class F implements K, H {
+
+    private double a = 100.500;
+
+    private long g = 4321;
+
+    public int hh() {
+        return new java.util.Random().nextInt();
+    }
+
+    public int[] ii() {
+        return new int[]{4, 3, 2, 1};
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public int ae() {
+        return 9;
+    }
+
+    public long ac() {
+        return 333;
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+}
