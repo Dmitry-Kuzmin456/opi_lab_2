6ff61a5f9abf9df76dea2185ee3ce95ee098d711
red_user <red@mail>
2026-03-27 16:41:30 +0300

    r14

--- K.java
+++ K.java
@@ -1,6 +1,14 @@
-public interface K {
+public class K extends null {
 
     java.util.Random mm();
 
     int ae();
+
+    public java.lang.Class qq() {
+        return getClass();
+    }
+
+    public int[] ii() {
+        return new int[]{0, 1, 2, 3, 4};
+    }
 }
