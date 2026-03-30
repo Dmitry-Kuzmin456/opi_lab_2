b79c8a719e3784875fb0d0bb45016538b0607a13
red_user <red@mail>
2026-03-30 12:25:58 +0300

    r14

--- H.java
+++ H.java
@@ -1,6 +1,14 @@
-public interface H {
+public class H extends null {
 
     long ac();
 
     java.util.List<String> jj();
+
+    public String nn() {
+        return "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++";
+    }
+
+    public int cc() {
+        return 42;
+    }
 }
