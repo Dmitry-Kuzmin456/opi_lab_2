adc91b52f3bf1a92422d6297b0f11ad38ecc333d
red_user <red@mail>
2026-03-30 12:24:19 +0300

    r0

---
+++ B.java
@@ -0,0 +1,30 @@
+public class B implements K, H {
+
+    private byte k = 1;
+
+    private long b = 1234;
+
+    public long dd() {
+        return 33;
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public int ae() {
+        return java.lang.Math.abs(-6);
+    }
+
+    public long ac() {
+        return 333;
+    }
+
+    public java.util.List<String> jj() {
+        return new java.util.LinkedList<String>();
+    }
+}
